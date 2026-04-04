91c662b33853be598494b83a849a955c2fabb74b
stefan <stefan.nav7@gmail.com>
2026-04-04 09:38:30 -0600

    Fine-tune header icon/number sizing and spacing

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -7,16 +7,16 @@ export function HomeHeader() {
     <div className="flex items-center justify-center gap-6 px-6 pb-[10px] pt-[54px]">
       {/* Streak badge */}
       <div className="flex items-center gap-1.5">
-        <img src="/images/Leaf.svg" alt="" className="h-[16px] w-[14px]" />
-        <p className="text-[16px] font-semibold leading-[1.2] text-lm-text-primary">
+        <img src="/images/Leaf.svg" alt="" className="h-[18px] w-[16px]" />
+        <p className="text-[13px] font-semibold leading-none text-lm-text-primary">
           {streak}
         </p>
       </div>
 
       {/* Stars badge */}
       <div className="flex items-center gap-1.5">
-        <img src="/images/Star.svg" alt="" className="h-[16px] w-[17px]" />
-        <p className="text-[16px] font-semibold leading-[1.2] text-lm-text-primary">
+        <img src="/images/Star.svg" alt="" className="h-[18px] w-[19px]" />
+        <p className="text-[13px] font-semibold leading-none text-lm-text-primary">
           {foundationStars}
         </p>
       </div>
